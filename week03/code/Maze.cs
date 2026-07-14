57470539154b1efa2d463324f7b165adf44d95a9
Caio Palladino Da Rosa <caiopalladinodarosa@MacBook-Neo-de-Caio.local>
2026-07-14 12:29:07 -0300

    Complete Week 03: Sets and Maps assignment

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -1,18 +1,8 @@
+using System;
+using System.Collections.Generic;
+
 /// <summary>
-/// Defines a maze using a dictionary. The dictionary is provided by the
-/// user when the Maze object is created. The dictionary will contain the
-/// following mapping:
-///
-/// (x,y) : [left, right, up, down]
-///
-/// 'x' and 'y' are integers and represents locations in the maze.
-/// 'left', 'right', 'up', and 'down' are boolean are represent valid directions
-///
-/// If a direction is false, then we can assume there is a wall in that direction.
-/// If a direction is true, then we can proceed.  
-///
-/// If there is a wall, then throw an InvalidOperationException with the message "Can't go that way!".  If there is no wall,
-/// then the 'currX' and 'currY' values should be changed.
+/// Defines a maze using a dictionary.
 /// </summary>
 public class Maze
 {
@@ -25,41 +15,40 @@ public class Maze
         _mazeMap = mazeMap;
     }
 
-    // TODO Problem 4 - ADD YOUR CODE HERE
-    /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
     public void MoveLeft()
     {
-        // FILL IN CODE
+        // Posição 0 do array representa a esquerda
+        if (_mazeMap[(_currX, _currY)][0])
+            _currX--;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
-    /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
     public void MoveRight()
     {
-        // FILL IN CODE
+        // Posição 1 do array representa a direita
+        if (_mazeMap[(_currX, _currY)][1])
+            _currX++;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
-    /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
     public void MoveUp()
     {
-        // FILL IN CODE
+        // Posição 2 do array representa cima
+        if (_mazeMap[(_currX, _currY)][2])
+            _currY--;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
-    /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
     public void MoveDown()
     {
-        // FILL IN CODE
+        // Posição 3 do array representa baixo
+        if (_mazeMap[(_currX, _currY)][3])
+            _currY++;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
     public string GetStatus()
