e596ba550bae6edcd444345f13185131cd2956c4
Caio Palladino Da Rosa <caiopalladinodarosa@MacBook-Neo-de-Caio.local>
2026-08-05 15:16:51 -0300

    Solução completa da designação da Semana 06 - Trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class Node
 {
     public int Data { get; set; }
@@ -12,6 +14,10 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+        {
+            return;
+        }
 
         if (value < Data)
         {
@@ -34,12 +40,20 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
